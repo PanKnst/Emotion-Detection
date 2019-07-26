d08551da4023da77340c86e0e6509c6d07037aa6
Panagiotis Konstantinou <panknst@gmail.com>
2019-07-26 11:02:18 +0100

    Video playback using OpenCV

--- faceDetection.py
+++ faceDetection.py
@@ -1,1 +1,24 @@
-#Testing github integration
+"""First test with openCV video playback from camera
+Taken from OpenCV website and commented out a few lines of code """
+
+
+#import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
